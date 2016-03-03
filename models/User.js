3a464df732cb38f0461b213ee15e7fe862fd0c39
Yael Amir <yaelamir1@gmail.com>
2016-03-03 15:57:36 -0800

    clean up user file

---
+++ models/User.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+
+// SCHEMA
+var userSchema = new mongoose.Schema({
+  name:      { type: String, required: true },
+  email:     { type: String, required: true },
+  // password:  String,
+  moderator: { type: Boolean, default: false }
+});
+// MODEL
+var User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
+
+
